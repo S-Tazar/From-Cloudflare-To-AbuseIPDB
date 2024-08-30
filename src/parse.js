6658decb8c676f1b6b9e53e05644ade6c13246bd
S-Tazar <starslookdown@qq.com>
2024-08-30 15:50:53 +0800

    [Feat] add parse.js to drop duplicates and filter sensitive words

---
+++ src/parse.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+// 替换敏感词
+function filterSensitive(someArray) {
+    const sensitiveWords = JSON.parse(process.env.SENSITIVE);
+    console.log(sensitiveWords);
+    someArray.forEach(item => {
+        item.clientRequestPath = sensitiveWords.some(word => item.clientRequestPath.includes(word))
+            ? `CloudFlare WAF REPORT: Disobey robots.txt. Suspicious web crawler.`
+            : `CloudFlare WAF REPORT: ${item.clientRequestPath}`;
+
+        item.clientRequestPath = item.clientRequestQuery ? `${item.clientRequestPath}${item.clientRequestQuery}` : item.clientRequestPath;
+    });
+    return someArray;
+}
+
+
+function dropDuplicates(jsonArray) {
+    // object keys must not be repeated
+    const uniqueByName = Object.values(
+        jsonArray.reduce((item, obj) => {
+            item[obj.clientIP] = obj; 
+            return item;
+        }, {})
+    );
+    return uniqueByName;
+}
+
+
+module.exports = {
+    dropDuplicates,
+    filterSensitive,
+}
