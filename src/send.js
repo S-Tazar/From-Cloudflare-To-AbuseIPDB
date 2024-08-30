a8affdb0e765f49473361ab1b1c8fbec549b1429
S-Tazar <starslookdown@qq.com>
2024-08-30 15:45:32 +0800

    [Feat] add send.js for sending message to AbuseIPDB

---
+++ src/send.js
@@ -0,0 +1,31 @@
+const fetch = require("node-fetch");
+const FormData = require('form-data');
+require("dotenv").config();
+
+function sendAlertToAbuseIPDB(someArray) {
+    someArray.forEach(item => {
+        const formData = new FormData();
+        formData.append('ip', item.clientIP);
+        formData.append('categories', "19, 21");
+        formData.append('comment', item.clientRequestPath);
+
+        // console.log(item.clientIP);
+        options = {
+            method: "POST",
+            headers:  {"Accept": "application/json", "Key": process.env.ABUSEIP_DB_KEY},
+            body: formData,
+        };
+        console.log(options);
+
+        fetch("https://api.abuseipdb.com/api/v2/report", options)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+        })
+        .catch(err => console.error(err));
+    });
+}
+
+module.exports = {
+    sendAlertToAbuseIPDB,
+}
